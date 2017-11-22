b2ff6888dbe2eeadc4cf568c490482aaf23924df
majun <needyousir@qq.com>
2017-11-22 11:43:26 +0800

    new admin template

---
+++ src/views/new.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="animated fadeIn">
+  	<Row>
+  		<Col :sm="24" :md="24" :lg="12">
+  			<h3>11</h3>
+  			<p>22</p>
+  		</Col>
+  	</Row>
+  </div>
+</template>
+<script>
+	export default{
+		data(){
+			return{}
+		}
+	}
+</script>
